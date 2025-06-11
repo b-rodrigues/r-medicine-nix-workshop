3c8150d821bbc9159fcd375bea4c65a4fef90e38
Bruno Rodrigues <bruno@brodrigues.co>
2025-06-11 09:23:54 +0200

    moved slides

--- _targets.R
+++ _targets.R
@@ -4,7 +4,7 @@ library(tarchetypes)
 list(
   tar_force(
     name = pres,
-    command = quarto::quarto_render("slides/index.qmd"),
+    command = quarto::quarto_render("index.qmd"),
     format = "file",
     force = TRUE
   )
